f124063af41e223704c7a39f3001a59951586582
Narein Narayan Boddapati <boddapnn@mail.uc.edu>
2025-01-29 11:20:22 -0500

    Add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { HomeComponent } from './main/home/home.component';
 import { AboutComponent } from './main/about/about.component';
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
-    {path: '**', component: HomeComponent}
+    {path: 'Home', component: HomeComponent},
+    {path: 'About', component: AboutComponent},
 ];
