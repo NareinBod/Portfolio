090466961175dae11a69df25e2c4d11216ff9eff
Narein Narayan Boddapati <boddapnn@mail.uc.edu>
2025-01-29 10:59:18 -0500

    changed styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { HomeComponent } from './main/home/home.component';
 import { AboutComponent } from './main/about/about.component';
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'About', component: AboutComponent},
+    {path: 'about', component: AboutComponent},
+    {path: '**', component: HomeComponent}
 ];
