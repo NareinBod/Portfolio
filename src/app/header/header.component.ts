710d3911b4bcd3bb7ee71c89323b883dd794a74e
Narein Narayan Boddapati <boddapnn@mail.uc.edu>
2025-01-28 22:34:31 -0500

    Add images

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  symbol: string = '🌞';
 
+  onClick() {
+    if(document.body.style.backgroundColor === ' #0a192f') {
+      this.symbol = '🌙';
+      document.body.style.backgroundColor = 'white';
+    }
+    else if(document.body.style.backgroundColor === 'white') {
+      this.symbol = '🌞';
+      document.body.style.backgroundColor = '#0a192f';
+    }
+  }
 }
