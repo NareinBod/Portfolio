1d4742fda112eecf91388fedd65f543968d4539f
Narein Narayan Boddapati <boddapnn@mail.uc.edu>
2025-01-29 12:34:37 -0500

    Add Contact component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './main/home/home.component';
 import { AboutComponent } from './main/about/about.component';
 import { ExperienceComponent } from './main/experience/experience.component';
+import { ContactComponent } from './main/contact/contact.component';
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'Home', component: HomeComponent},
     {path: 'About', component: AboutComponent},
     {path: 'Experience', component: ExperienceComponent},
+    {path: 'Contact', component: ContactComponent}
 ];
